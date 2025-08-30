dc3e4e19323037df8438375e7f660ac531104e8d
amin-ep <amincode24@gmail.com>
2025-08-30 13:40:36 +0330

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const manifestIcons = [
+  {
+    src: "/Logo.png",
+    sizes: "192*192",
+    type: "image/png",
+  },
+];
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Tic Tac Toe",
+        short_name: "T T T",
+        icons: manifestIcons,
+      },
+    }),
+  ],
 });
